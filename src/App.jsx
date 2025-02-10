6e9976cbfd572572cb695f406da208563fc06e31
Miguel Edward A. Rosal <miguelrosal12@gmail.com>
2025-02-10 10:36:12 +0800

    Working birthday

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Home,Picture,Card,Cake,Present } from "./components"
+import { Home, Picture, Card, Cake, Present } from "./components"
 import './index.css'
 
 function App() {
@@ -7,11 +7,11 @@ function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/pictures" element={<Picture/>}/>
-          <Route path="/card" element={<Card/>}/>
-          <Route path="/cake" element={<Cake/>}/>
-          <Route path="/present" element={<Present/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/pictures" element={<Picture />} />
+        <Route path="/card" element={<Card />} />
+        <Route path="/cake" element={<Cake />} />
+        <Route path="/present" element={<Present />} />
       </Routes>
 
     </Router>
